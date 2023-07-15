8435899bf0264db90ee33c60ba7cc2164036b0d4
VitalijusKiubis <vitalijus.kiubis@gmail.com>
2023-07-15 23:42:11 +0300

    Initial commit

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,12 +1,12 @@
 import pytest
-from mccalcy import calculator
+from mccalcy import mccalcy
 
 
 def test_add() -> None:
     """
     Test the add() method.
     """
-    calc = calculator.Calculator()
+    calc = mccalcy.Calculator()
     calc.add(10)
     assert calc.memory == 10
 
@@ -15,7 +15,7 @@ def test_subtract() -> None:
     """
     Test the subtract() method.
     """
-    calc = calculator.Calculator()
+    calc = mccalcy.Calculator()
     calc.add(10)
     calc.subtract(5)
     assert calc.memory == 5
@@ -25,7 +25,7 @@ def test_multiply() -> None:
     """
     Test the multiply() method.
     """
-    calc = calculator.Calculator()
+    calc = mccalcy.Calculator()
     calc.add(10)
     calc.multiply(2)
     assert calc.memory == 20
@@ -35,7 +35,7 @@ def test_divide() -> None:
     """
     Test the divide() method.
     """
-    calc = calculator.Calculator()
+    calc = mccalcy.Calculator()
     calc.add(10)
     calc.divide(2)
     assert calc.memory == 5
@@ -45,7 +45,7 @@ def test_root() -> None:
     """
     Test the root() method.
     """
-    calc = calculator.Calculator()
+    calc = mccalcy.Calculator()
     calc.add(16)
     calc.root(2)
     assert calc.memory == 4
@@ -55,7 +55,7 @@ def test_reset_memory() -> None:
     """
     Test the reset_memory() method.
     """
-    calc = calculator.Calculator()
+    calc = mccalcy.Calculator()
     calc.add(10)
     calc.reset_memory()
     assert calc.memory == 0
